2f0f056b86cb791f871ae959652305051c261f09
Jef-king <147884623+Jef-king@users.noreply.github.com>
2024-05-18 21:22:44 +0530

    Add files via upload
    all done

---
+++ script.js
@@ -0,0 +1,36 @@
+let count = document.querySelector("#count");
+let error = document.querySelector(".error");
+let decrement=document.querySelector("#decrement");
+let increment=document.querySelector("#increment");
+let clear=document.querySelector("#clear");
+let x=0;
+decrement.addEventListener("click",()=>{
+    x--;
+    
+    if(x<0){
+        decrement.disabled=true;
+        error.innerHTML="Error : Cannot go below 0";
+        x=0;
+
+    }else{
+        count.innerText=x;
+    }
+    if(x==0){
+        clear.style.display="none";
+    }
+});
+increment.addEventListener("click",()=>{
+    x++;
+    count.innerText=x;
+    if(x==1){
+        decrement.disabled=false;
+        error.innerHTML="";
+        clear.style.display="inline";
+
+    }
+});
+clear.addEventListener("click",()=>{
+    x=0;
+    count.innerText=x;
+    clear.style.display="none";
+});
